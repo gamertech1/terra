deee0ef019e440bfeb9298838a2cb959f6ec1e16
gamertech1 <97377147+gamertech1@users.noreply.github.com>
2025-01-05 11:25:24 +0530

    Add files via upload

---
+++ modules/vm/main.tf
@@ -0,0 +1,56 @@
+resource "azurerm_virtual_network" "example" {
+  name                = "example-vnet"
+  location            = var.location
+  resource_group_name = var.resource_group
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "example-subnet"
+  virtual_network_name = azurerm_virtual_network.example.name
+  resource_group_name  = var.resource_group
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = var.location
+  resource_group_name = var.resource_group
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_virtual_machine" "example" {
+  name                  = "example-vm"
+  location              = var.location
+  resource_group_name   = var.resource_group
+  network_interface_ids = [azurerm_network_interface.example.id]
+  vm_size               = var.size
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name              = "os-disk"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  os_profile {
+    computer_name  = "examplevm"
+    admin_username = "azureuser"
+    admin_password = "P@ssw0rd1234!" # Use SSH keys in production
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+}
+
